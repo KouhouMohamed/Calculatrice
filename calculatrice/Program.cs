ba33ee6bd1d437c4e1eea0acdaa8a762da6a1930
MKOUHOU <m.kouhou@bouygues-constrution.com>
2022-07-08 16:21:39 +0100

    first commit

---
+++ calculatrice/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+double calculate(double a, double b, char operation)
+{
+    switch (operation)
+    {
+        case '+':
+            return a + b;
+        case '-':   
+            return a - b;
+        case '*':
+            return a * b;
+        case '/':
+            return a / b;
+        default:
+            Console.WriteLine("The operation entred is not correcte");
+            return 0;
+    }
+}
+double a;
+double b;
+char operation;
+
+Console.Write("Enter the fisrt number a = ");
+a = double.Parse(Console.ReadLine());
+
+Console.Write("Enter the second number b = ");
+b = double.Parse(Console.ReadLine());
+
+Console.Write("Enter the operation symbole : ");
+operation = Char.Parse(Console.ReadLine());
+
+
+
+Console.WriteLine($"The result of {a} {operation} {b} = {calculate(a, b, operation)}");
+
